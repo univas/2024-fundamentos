b90b907f574ce2dc60fc27ce398f33a58f8abeaf
Marcos Antônio dos Santos <marcaosi2014@gmail.com>
2024-08-13 21:33:10 -0300

    Add files via upload

---
+++ entrada_de_dados/exercicio_resolvido.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+    Crie um programa que solicite ao usuário
+    digitar o valor de um produto e o percentual
+    de desconto concedido.
+    O programa deve calcular o valor do desconto
+    e apresentar o valor do produto com desconto.
+*/
+
+int main(){
+    int valor_produto;
+    int valor_desconto;
+    float valor_com_desconto, desconto;
+
+    printf("Valor do produto:");
+    scanf("%d", &valor_produto);
+
+    printf("\nValor do desconto:");
+    scanf("%d", &valor_desconto);
+
+    desconto = (valor_desconto / 100.0) * valor_produto;
+    valor_com_desconto = valor_produto - desconto;
+
+    printf("\n\n\tO valor com desconto eh: %.f", valor_com_desconto);
+
+}
